be4569814e16b32db097bd0499fa1949fa3226dd
AngomRanjan <angomranjan@gmail.com>
2022-09-09 16:56:24 +0530

    Add ShowInfo Component

---
+++ src/components/ShowInfo.js
@@ -0,0 +1,35 @@
+import '../css/Shows.css';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import Show from './Show';
+import { getShowInfoAction } from '../redux/showInfo';
+
+const ShowInfo = () => {
+  const { id } = useParams();
+  const showInfo = useSelector((state) => state.showInfo);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getShowInfoAction(id));
+  }, []);
+
+  return (
+    <div
+      className="showInfo"
+    >
+      {(showInfo) && (
+        <Show
+          key={showInfo.id}
+          id={showInfo.id}
+          name={showInfo.name}
+          image={showInfo.image.medium}
+          rating={showInfo.rating.average}
+        />
+      )}
+      <h1>{showInfo.name}</h1>
+    </div>
+  );
+};
+
+export default ShowInfo;
